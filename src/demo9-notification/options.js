d2b0b79eebe4e4decdd1cb35f4afcb48e8a61358
zhangaz1 <zhangaz1@hotmail.com>
2018-10-30 14:58:31 +0800

    demo9 works

--- src/demo9-notification/options.js
+++ src/demo9-notification/options.js
@@ -7,27 +7,29 @@
   field.
 */
 function ghost(isDeactivated) {
-  options.style.color = isDeactivated ? 'graytext' : 'black';
-                                              // The label color.
-  options.frequency.disabled = isDeactivated; // The control manipulability.
+    options.style.color = isDeactivated ? 'graytext' : 'black';
+    // The label color.
+    options.frequency.disabled = isDeactivated; // The control manipulability.
 }
 
 window.addEventListener('load', function() {
-  // Initialize the option controls.
-  options.isActivated.checked = JSON.parse(localStorage.isActivated);
-                                         // The display activation.
-  options.frequency.value = localStorage.frequency;
-                                         // The display frequency, in minutes.
+    // Initialize the option controls.
+    options.isActivated.checked = JSON.parse(localStorage.isActivated);
+    // The display activation.
+    options.frequency.value = localStorage.frequency;
+    // The display frequency, in minutes.
 
-  if (!options.isActivated.checked) { ghost(true); }
+    if (!options.isActivated.checked) {
+        ghost(true);
+    }
 
-  // Set the display activation and frequency.
-  options.isActivated.onchange = function() {
-    localStorage.isActivated = options.isActivated.checked;
-    ghost(!options.isActivated.checked);
-  };
+    // Set the display activation and frequency.
+    options.isActivated.onchange = function() {
+        localStorage.isActivated = options.isActivated.checked;
+        ghost(!options.isActivated.checked);
+    };
 
-  options.frequency.onchange = function() {
-    localStorage.frequency = options.frequency.value;
-  };
+    options.frequency.onchange = function() {
+        localStorage.frequency = options.frequency.value;
+    };
 });
