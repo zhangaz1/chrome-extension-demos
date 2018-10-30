13769a6c7c30a32de93a8ec3e4780977ecb9eb31
zhangaz1 <zhangaz1@hotmail.com>
2018-10-30 16:22:54 +0800

    add cVim code

---
+++ src/ext_cVim/background_scripts/links.js
@@ -0,0 +1,10 @@
+var Links = {};
+
+Links.multiOpen = function(links) {
+    links.forEach(function(item) {
+        chrome.tabs.create({
+            url: item,
+            active: false
+        });
+    });
+};
