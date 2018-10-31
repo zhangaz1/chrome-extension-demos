d504ed63a6aa7e3bebb2c6128d49ae68f9769924
zhangaz1 <zhangaz1@hotmail.com>
2018-10-31 10:44:36 +0800

    run init

--- src/my_tabsManager/src/popup/popup.js
+++ src/my_tabsManager/src/popup/popup.js
@@ -1,3 +1,32 @@
-var bg = chrome.extension.getBackgroundPage();
+;
+(function() {
+    let bg = chrome.extension.getBackgroundPage();
 
-console.log('xxxxxxxxx', bg.tabs);
+    $(init);
+
+    return void(0);
+
+    function init() {
+        bindTabs(bg.tabs);
+    }
+
+    function bindTabs(tabs) {
+        let tabRowTemplate = getTabRowTemplate();
+        let template = _.template(tabRowTemplate);
+        let html = template(tabs);
+
+        $('#tabs')
+            .empty()
+            .append(html);
+    }
+
+    function getTabRowTemplate() {
+        return `<tr>
+			<td>{{id}}</td>
+			<td>{{title}}</td>
+			<td>{{url}}</td>
+			<td><button tabId="{{id}}">Close</button></td>
+		</tr>`;
+    }
+
+})();
